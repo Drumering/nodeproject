7de6eadcb034451d2e6fad83f1b18d51ed152846
Anderson Mendes de Almeida <ama@cesar.org.br>
2020-10-08 21:17:33 -0300

    [TASK]Added tags feature, image upload and resize

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -9,10 +9,26 @@ exports.userMiddleware = (req, res, next)=> {
 
 exports.index = async (req, res)=>{
     let responseJson = {
-        pageTitle: 'HOME'
+        pageTitle: 'HOME',
+        posts: [],
+        tags: [],
+        tag: ''
     };
+
+    responseJson.tag = req.query.t;
+
+    const tags = await Post.getTagsList();
+    for (let i in tags) {
+        if(tags[i]._id == responseJson.tag){
+            tags[i].class = "selected"
+        }
+    }
+    responseJson.tags = tags;
+
+    console.log(tags);
 
     const posts = await Post.find();
     responseJson.posts = posts;
+
     res.render('home', responseJson);
 };
